9167aacf2340b00abba41338b43671162e6a85f5
Maximilian Schuele <m.schuele@tum.de>
2023-01-12 13:55:52 +0100

    run benchmarks: automate csv processing

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -3,6 +3,9 @@
 export LD_LIBRARY_PATH=../umbra/lib/
 UMBRASQL=../umbra/bin/sql
 
+rm -f iris/gd_nn.csv
+rm -f mnist/gd_mnist.csv
+
 # run python baseline
 (cd iris && python3 iris_bench.py > numpy_nn.csv)
 (cd mnist && python3 mnist_bench.py > numpy_mnist.csv)
@@ -13,8 +16,12 @@ UMBRASQL=../umbra/bin/sql
 (cd mnist && ./mnist_sql92_bench.sh | $UMBRASQL)
 
 # PSQL (takes time)
+echo "create or replace function sig(x float) returns float as 'select 1::float/(1+exp(-x))' LANGUAGE 'sql';" | psql
 (cd iris && ./iris_sql92_psql_bench.sh | psql)
 
 # create plots
+sed -i 's/execution_time_min,/execution_time,/' mnist/gd_mnist.csv iris/gd_nn.csv
+sed -i 's/name,/name,atts,limit,lr,iter,threads,/' mnist/gd_mnist.csv iris/gd_nn.csv
+
 (cd iris && Rscript iris.r)
 (cd mnist && Rscript mnist.r)
