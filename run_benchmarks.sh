de7a5c908e4752cabe772924f35c4e3e8158b7cb
Maximilian Schuele <m.schuele@tum.de>
2023-01-12 12:35:02 +0000

    restructure run benchmark

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -3,14 +3,18 @@
 export LD_LIBRARY_PATH=../umbra/lib/
 UMBRASQL=../umbra/bin/sql
 
-cd iris
-python3 iris_bench.py > numpy_nn.csv
-./iris_sql92_bench.sh | $UMBRASQL
-./iris_sql92_psql_bench.sh | psql
-Rscript iris.r
+# run python baseline
+(cd iris && python3 iris_bench.py > numpy_nn.csv)
+(cd mnist && python3 mnist_bench.py > numpy_mnist.csv)
 
-cd ../mnist
-python3 mnist_bench.py > numpy_mnist.csv
-./mnist_sql92_bench.sh | $UMBRASQL
-$UMBRASQL < mnist_bench.sql
-Rscript mnist.r
+# run Umbra
+(cd iris && ./iris_sql92_bench.sh | $UMBRASQL)
+(cd mnist && ./mnist_sql92_bench.sh | $UMBRASQL)
+(cd mnist && $UMBRASQL < mnist_bench.sql)
+
+# PSQL (takes time)
+(cd iris && ./iris_sql92_psql_bench.sh | psql)
+
+# create plots
+(cd iris && Rscript iris.r)
+(cd mnist && Rscript mnist.r)
